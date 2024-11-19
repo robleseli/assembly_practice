294f7031070e365643814b101d8c89dd155c8e50
Eli Robles <97868399+robleseli@users.noreply.github.com>
2024-11-19 15:12:14 -0600

    Create practice_5.asm

---
+++ practice_5.asm
@@ -0,0 +1,3 @@
+label:      .fill   128         @ reserves 128 bytes
+label2:     .fill   128, 2, 1   @ reserves 128 2-byte blocks (256 bytes) 
+                                @ with each 2-byte value equal to 1
